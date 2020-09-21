1a1ef006274540637cd38d5c38851ed4cf61cecc
iAryslanov <Aryslanov@gmail.com>
2020-09-21 13:48:08 +0300

    Initial commit

---
+++ HandsDo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HandsDo
+//
+//  Created by Igor Aryslanov on 21.09.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
