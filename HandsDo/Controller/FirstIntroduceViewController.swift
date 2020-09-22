a09f692fcbb0b5593da508fda703f2841a324981
iAryslanov <Aryslanov@gmail.com>
2020-09-22 13:53:54 +0300

    added UI to FirstIntroduceViewController

--- HandsDo/Controller/FirstIntroduceViewController.swift
+++ HandsDo/Controller/FirstIntroduceViewController.swift
@@ -8,11 +8,19 @@
 import UIKit
 
 class FirstIntroduceViewController: UIViewController {
-
+    
+    @IBAction func nextButton(_ sender: Any) {
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+        navigationController?.navigationBar.barTintColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+        navigationItem.title = "Как мы работаем"
+        let textAttributes = [NSAttributedString.Key.foregroundColor: UIColor.white]
+        navigationController?.navigationBar.titleTextAttributes = textAttributes
     }
     
 
