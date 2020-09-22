6f6b6e9a44fdcba2cec9229a6e088c12618d7536
iAryslanov <Aryslanov@gmail.com>
2020-09-22 10:51:50 +0300

    add FirstIntroduceViewController

---
+++ HandsDo/Controller/FirstIntroduceViewController.swift
@@ -0,0 +1,29 @@
+//
+//  FirstIntroduceViewController.swift
+//  HandsDo
+//
+//  Created by Igor Aryslanov on 22.09.2020.
+//
+
+import UIKit
+
+class FirstIntroduceViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
